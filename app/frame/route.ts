2c7b95b96723cca30b628ca75508d211e06a64c2
funkornaut001 <funkornaut@gmail.com>
2024-03-17 16:49:02 -0500

    init

---
+++ app/frame/route.ts
@@ -0,0 +1,58 @@
+import { NextRequest, NextResponse } from "next/server"
+import { getConnectedAddressForUser } from "../utils/fc";
+import { mintNft, balanceOf } from "../utils/mint";
+
+import { PinataFDK } from "pinata-fdk";
+const fdk = new PinataFDK({
+  pinata_jwt: process.env.PINATA_JWT as string,
+  pinata_gateway: process.env.GATEWAY_URL as string,
+});
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  try {
+    const frameMetadata = await fdk.getFrameMetadata({
+      post_url: `${process.env.BASE_URL}/frame`,
+      buttons: [{ label: "Mint NFT", action: "post" }], // can probaly add in regenerate button to make new blob
+      aspect_ratio: "1:1",
+      cid: "QmSYN7KT847Nado3fxFafYZgG6NXTMZwbaMvU9jhu5nPmJ", // likely need to change this
+    });
+    return new NextResponse(frameMetadata);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: error });
+  }
+}
+
+// mint request
+export async function POST(req: NextRequest, res: NextResponse) {
+  const body = await req.json();
+  const fid = body.untrustedData.fid; //get farcaster id
+  const address = await getConnectedAddressForUser(fid); //get users wallet from farcaster
+  const balance = await balanceOf(address); //do they own any NFTs
+    console.log(balance);
+    //they can mint only 1 nft
+  if (typeof balance === "number" && balance !== null && balance < 1) {
+    try {
+      const mint = await mintNft(address);
+      console.log(mint);
+      const frameMetadata = await fdk.getFrameMetadata({
+        post_url: `${process.env.BASE_URL}/redirect`,
+        buttons: [{ label: "Learn How to Make This", action: "post_redirect" }],
+        aspect_ratio: "1:1",
+        cid: "QmUx3kQH4vR2t7mTmW3jHJgJgJGxjoBsMxt6z1fkZEHyHJ",
+      });
+      return new NextResponse(frameMetadata);
+    } catch (error) {
+      console.log(error);
+      return NextResponse.json({ error: error });
+    }
+  } else {
+    const frameMetadata = await fdk.getFrameMetadata({
+      post_url: `${process.env.BASE_URL}/redirect`,
+      buttons: [{ label: "Learn How to Make This", action: "post_redirect" }],
+      aspect_ratio: "1:1",
+      cid: "QmaaEbtsetwamJwfFPAQAFC6FAE1xeYsvF7EBKA8NYMjP2",
+    });
+    return new NextResponse(frameMetadata);
+  }
+}
---
+++ app/redirect/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest, res: NextResponse){
+    try {
+      //@todo make a better redirect
+   return NextResponse.redirect('https://pinata.cloud', { status: 302 }) 
+  } catch (error) {
+   console.log(error) 
+    return NextResponse.json({ error: error })
+  }
+}
